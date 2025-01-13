17ab6dfcd8c791cb32bf0a2369a291d55e599e73
MgKyaw <mgkyaw20112@gmail.com>
2025-01-13 20:30:23 +0700

    feat: Retrieve data from Elasticsearch

--- ElasticSearch.NetCore/Controllers/HomeController.cs
+++ ElasticSearch.NetCore/Controllers/HomeController.cs
@@ -1,21 +1,37 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+using Elastic.Clients.Elasticsearch;
 using ElasticSearch.NetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace ElasticSearch.NetCore.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ElasticsearchClient _elasticsearchClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ElasticsearchClient elasticsearchClient)
     {
         _logger = logger;
+        _elasticsearchClient = elasticsearchClient;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var searchResponse = await _elasticsearchClient.SearchAsync<Book>(s => s
+            .Index("books")
+            .Query(q => q.MatchAll(new Elastic.Clients.Elasticsearch.QueryDsl.MatchAllQuery()))
+        );
+
+        if (searchResponse.IsValidResponse)
+        {
+            System.Console.WriteLine("Found.");
+        }
+        else
+        {
+            System.Console.WriteLine("Not Found.");
+        }
+        return View(searchResponse);
     }
 
     public IActionResult Privacy()
